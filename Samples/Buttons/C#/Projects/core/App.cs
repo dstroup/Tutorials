9e2b13247c0afadf90b08ec8b669d449c1dd99f5
Dustin Stroup <dstroup@cspeed.com>
2019-04-24 09:18:05 -0400

    Add Buttons-core sample.
    Note this is configured to use OpenGL.Net 0.8.4, which doesn't ship .NET Core/Standard libraries since the 0.7.1 netcoreapp builds had incorrect assembly version numbers.

---
+++ Samples/Buttons/C#/Projects/core/App.cs
@@ -0,0 +1,49 @@
+﻿using NoesisApp;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Buttons
+{
+    partial class App : Application
+    {
+        protected override Display CreateDisplay()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                return new XDisplay();
+            }
+            else if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return new Win32Display();
+            }
+            else
+            {
+                throw new PlatformNotSupportedException();
+            }
+        }
+
+        protected override RenderContext CreateRenderContext()
+    {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                return new RenderContextGLX();
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return new RenderContextWGL();
+            }
+            else
+            {
+                throw new PlatformNotSupportedException();
+            }
+        }
+        static void Main(string[] args)
+        {
+            App app = new App();
+            app.Uri = "App.xaml";
+            app.Run();
+        }
+    }
+}
